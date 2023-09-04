cf90a9e711c966f3a96f83b1db43b6437fb96ebb
Olga Demina <misertylab.r6s@gmail.com>
2023-09-04 21:42:59 +0300

    added second test

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -4,6 +4,8 @@ import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
@@ -19,5 +21,23 @@ public class SearchTests extends TestBase {
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/search_container"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    void unsuccessfulArticleOpenTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/search_container"))
+                        .shouldHave(sizeGreaterThan(0)));
+        step("Open article", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container")).first().click()
+        );
+        step("Verify results", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_button")).shouldBe(visible);
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
+        });
     }
 }
